d39ae2d8ad6656d97b6e25566fe6bf748e1c0b21
Mosil <mosil.biz@gmail.com>
2021-09-04 17:56:30 +0800

    Add DataStoreRepo in project

---
+++ app/src/main/java/space/mosil/workshop/datastore/DataStoreRepo.kt
@@ -0,0 +1,44 @@
+package space.mosil.workshop.datastore
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+class DataStoreRepo(private val dataStore: DataStore<Preferences>) {
+
+  private object PreferencesKeys {
+    val ACCOUNT = stringPreferencesKey("account")
+    val ID = stringPreferencesKey("device_id")
+    val TIME = longPreferencesKey("time")
+  }
+
+  val userDataFlow: Flow<UserData> = dataStore.data.catch { exception ->
+    if (exception is IOException) {
+      emit(emptyPreferences())
+    } else {
+      throw exception
+    }
+  }.map { preferences ->
+    val account = preferences[PreferencesKeys.ACCOUNT] ?: ""
+    val id = preferences[PreferencesKeys.ID] ?: ""
+    UserData(account, id)
+  }
+
+  suspend fun login(userData: UserData) {
+    dataStore.edit { preferences ->
+      preferences[PreferencesKeys.ACCOUNT] = userData.name
+      preferences[PreferencesKeys.ID] = userData.name
+      preferences[PreferencesKeys.TIME] = userData.modifyTime
+    }
+  }
+
+  suspend fun logout() {
+    dataStore.edit { preferences ->
+      preferences.clear()
+    }
+  }
+}
--- app/src/main/java/space/mosil/workshop/datastore/MainActivity.kt
+++ app/src/main/java/space/mosil/workshop/datastore/MainActivity.kt
@@ -1,12 +1,29 @@
 package space.mosil.workshop.datastore
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.datastore.preferences.SharedPreferencesMigration
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.lifecycleScope
 import com.google.android.material.textfield.TextInputEditText
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import java.util.*
+
+private const val DATA_PREFERENCES_NAME = "data_preferences"
+
+private val Context.dataStore by preferencesDataStore(
+  name = DATA_PREFERENCES_NAME,
+  produceMigrations = { context ->
+    listOf(SharedPreferencesMigration(context, DATA_PREFERENCES_NAME))
+  }
+)
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +35,9 @@ class MainActivity : AppCompatActivity() {
 
   lateinit var repo: DataRepo
 
+  private val dsRepo = DataStoreRepo(dataStore)
+  private val dsRepoFlow = dsRepo.userDataFlow
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
@@ -27,8 +47,6 @@ class MainActivity : AppCompatActivity() {
     btnGenerateDeviceId = findViewById(R.id.btn_main_generate_device_id)
     btnSave = findViewById(R.id.btn_main_save)
     btnClear = findViewById(R.id.btn_main_clear)
-
-    repo = DataRepo(this)
 
     setData()
 
@@ -54,15 +72,20 @@ class MainActivity : AppCompatActivity() {
   }
 
   private fun saveUser(account: String) {
-    val user = UserData(account, txtDeviceId.text.toString())
-    repo.setUser(user)
+    lifecycleScope.launch(Dispatchers.IO) {
+      val user = UserData(account, txtDeviceId.text.toString())
+      dsRepo.login(user)
+    }
   }
 
   private fun setData() {
-    val user = repo.getUser()
-    if (user.isLogin()) {
-      edtAccount.setText(user.name)
-      txtDeviceId.setText(user.deviceId)
+    lifecycleScope.launch(Dispatchers.IO) {
+      dsRepoFlow.collect { user ->
+        if (user.isLogin()) {
+          edtAccount.setText(user.name)
+          txtDeviceId.text = user.deviceId
+        }
+      }
     }
   }
 }
