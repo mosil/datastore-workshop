f5935e67f393f88cb26af830b8b4241f705bc063
Mosil <mosil.biz@gmail.com>
2021-09-04 16:55:56 +0800

    Implementation: Get Data From SharedPreferences

--- app/src/main/java/space/mosil/workshop/datastore/MainActivity.kt
+++ app/src/main/java/space/mosil/workshop/datastore/MainActivity.kt
@@ -2,9 +2,11 @@ package space.mosil.workshop.datastore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatTextView
 import com.google.android.material.textfield.TextInputEditText
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +15,8 @@ class MainActivity : AppCompatActivity() {
   lateinit var btnGenerateDeviceId: AppCompatButton
   lateinit var btnSave: AppCompatButton
   lateinit var btnClear: AppCompatButton
+
+  lateinit var repo: DataRepo
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -24,5 +28,41 @@ class MainActivity : AppCompatActivity() {
     btnSave = findViewById(R.id.btn_main_save)
     btnClear = findViewById(R.id.btn_main_clear)
 
+    repo = DataRepo(this)
+
+    setData()
+
+    btnGenerateDeviceId.setOnClickListener {
+      txtDeviceId.text = UUID.randomUUID().toString()
+    }
+
+    btnSave.setOnClickListener {
+      val account = edtAccount.text.toString()
+      when {
+        account.isEmpty() -> {
+          Toast.makeText(this, "帳號不可為空", Toast.LENGTH_LONG).show()
+        }
+        txtDeviceId.text.isEmpty() -> {
+          Toast.makeText(this, "Device 不可為空", Toast.LENGTH_LONG).show()
+        }
+        else -> {
+          saveUser(account)
+        }
+      }
+    }
+
+  }
+
+  private fun saveUser(account: String) {
+    val user = UserData(account, txtDeviceId.text.toString())
+    repo.setUser(user)
+  }
+
+  private fun setData() {
+    val user = repo.getUser()
+    if (user.isLogin()) {
+      edtAccount.setText(user.name)
+      txtDeviceId.setText(user.deviceId)
+    }
   }
 }
